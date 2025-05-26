c2563c46aef28e11c3ecef42a9defae0012fd075
Reza Aditya Pratama <115173282+Rezaadityap@users.noreply.github.com>
2025-05-26 10:01:38 +0700

    Update partners.js

--- src/data/partners.js
+++ src/data/partners.js
@@ -2,31 +2,31 @@ export const partnersLogo = [
   {
     id: 1,
     name: "Partner 1",
-    logo: "/logos/1.svg",
+    logo: "logos/1.svg",
   },
   {
     id: 2,
     name: "Partner 2",
-    logo: "/logos/2.svg",
+    logo: "logos/2.svg",
   },
   {
     id: 3,
     name: "Partner 3",
-    logo: "/logos/3.svg",
+    logo: "logos/3.svg",
   },
   {
     id: 4,
     name: "Partner 4",
-    logo: "/logos/1.svg",
+    logo: "logos/1.svg",
   },
   {
     id: 5,
     name: "Partner 5",
-    logo: "/logos/2.svg",
+    logo: "logos/2.svg",
   },
   {
     id: 6,
     name: "Partner 6",
-    logo: "/logos/3.svg",
+    logo: "logos/3.svg",
   },
 ];
